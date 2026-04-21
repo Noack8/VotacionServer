af44bbb840c74bd938004e9c4aa0bc420232e552
Noack8 <162505514+Noack8@users.noreply.github.com>
2026-04-21 17:45:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.send('¡Hola Mundo desde Vercel!');
 });
 
-app.get('/verificar', (req, res) => {
+app.post('/verificar', (req, res) => {
   const { votante } = req.query;
   try {
     const result = await pool.query('SELECT * FROM "ta7e41e2f119cd9d111c0738f2c71bd336467216b" WHERE "a89344c9c1c66bb7d5691d88a4b7309499e0324be" = $1', [votante]);
