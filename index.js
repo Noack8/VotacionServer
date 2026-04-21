5941ff6cd3efffc28c09ee9bf51d820402bc31ff
Noack8 <162505514+Noack8@users.noreply.github.com>
2026-04-21 17:44:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ app.get('/', (req, res) => {
 
 app.get('/verificar', (req, res) => {
   const { votante } = req.query;
-  const result = await pool.query('SELECT * FROM "ta7e41e2f119cd9d111c0738f2c71bd336467216b" WHERE "a89344c9c1c66bb7d5691d88a4b7309499e0324be" = $1', [votante]);
-  if (result.rows.length === 0) {
-    return res.status(200).json({ mensaje: 'Votante no encontrado puede votar' });
-  }
-  else {
-    return res.status(200).json({ mensaje: 'Votante encontrado, solo se puede votar una sola vez' });
+  try {
+    const result = await pool.query('SELECT * FROM "ta7e41e2f119cd9d111c0738f2c71bd336467216b" WHERE "a89344c9c1c66bb7d5691d88a4b7309499e0324be" = $1', [votante]);
+    if (result.rows.length === 0) {
+        return res.status(200).json({ mensaje: 'Votante no encontrado puede votar' });
+    }
+    else {
+        return res.status(200).json({ mensaje: 'Votante encontrado, solo se puede votar una sola vez' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
