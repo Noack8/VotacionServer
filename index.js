4b677b7aeea1e3b9b46b91c679b66491dd361728
Noack8 <162505514+Noack8@users.noreply.github.com>
2026-04-22 12:15:50 -0600

    Mejoras del frontEnd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 const path = require('path');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'indexV1.html'));
 });
 
 app.post('/verificar', async (req, res) => {
