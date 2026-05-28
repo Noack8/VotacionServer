9efb3c402eb8ed1878eaba3410a94343abbccab0
Noack8 <162505514+Noack8@users.noreply.github.com>
2026-05-27 22:42:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ app.post('/registrar-voto', async (req, res) => {
   // con los datos del voto solamente, esta informacion si va cifrada
   // con un cifrado simetrico AES-256-CBC. 
 
+    // Se insertan los valores de los votos en la BD
     try {
         const query1 = `
         INSERT INTO "public"."ta7e41e2f119cd9d111c0738f2c71bd336467216b" (
@@ -77,6 +78,7 @@ app.post('/registrar-voto', async (req, res) => {
         const values1 = [col2, col3, col4];
         const result1 = await pool.query(query1, values1);
 
+    // Se inserta el blockchain del votante y su trayetoria en la BD
         const query2 = `
         INSERT INTO "public"."t89344c9c1c66bb7d5691d88a4b7309499e0324be" (
             "a439145df693732a7d4567e33720a90124508ecdb",
@@ -98,42 +100,6 @@ app.post('/registrar-voto', async (req, res) => {
         });
     }
 });
-
-
-/*app.post('/votos', async (req, res) => {
-  const { col1, col2, col3, col4, col5, col6, col7 } = req.body;
-
-  if (!col1 || !col2 || !col3 || !col4 || !col5 || !col6 || !col7) {
-    return res.status(400).json({ 
-      error: 'Faltan campos: col1, col2, col3, col4, col5, col6, col7' 
-    });
-  }
-
-  try {
-    const query = `
-      INSERT INTO "public"."ta7e41e2f119cd9d111c0738f2c71bd336467216b" (
-        "a89344c9c1c66bb7d5691d88a4b7309499e0324be",
-        "a90457f37945b8dfb83e1b46c6e7e042e02d91984",
-        "a790d32ac3577c9048b63b8e56391bd0d63f9ecf4",
-        "a455ee7b3ef6288a901c9bca7593568c9dd36807a",
-        "a48a66962617bf51f46a9db9942af639056a5bc17",
-        "a79b70b410141467f65c2100cdb656a5cd3b58f53",
-        "ab99eb061f4f93aef0d9feaf32f7984afa9dd7c3b"
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7)
-      RETURNING id;
-    `;
-    const values = [col1, col2, col3, col4, col5, col6, col7];
-    const result = await pool.query(query, values);
-    res.status(201).json({ message: 'Insertado', id: result.rows[0].id });
-  } catch (error) {
-    console.error('ERROR DETALLADO:', error); 
-    res.status(500).json({ 
-      error: 'Error en la base de datos', 
-      detalle: error.message,  
-      codigo: error.code 
-    });
-  }
-});*/
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
